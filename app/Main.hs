8a6b321d267bdfa2dc89122955253280ba16c071
c4llv07e <kseandi@gmail.com>
2022-08-27 00:44:12 +0300

    simple main loop
    Signed-off-by: c4llv07e <kseandi@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,58 @@
 module Main where
 
+type Line = String
+type Text = [Line]
+
+data Editor = Editor { scrPos  :: Int
+                     , scrSize :: Int
+                     }
+
+stringToText :: String -> Text
+stringToText str = case dropWhile cond str of
+                     ""   -> []
+                     str' -> line : stringToText strTail
+                       where (line, strTail) = break cond str'
+                   where cond = (== '\n')
+
+showText :: Text -> IO ()
+showText [] = return ()
+showText text = do
+  putStrLn $ head text
+  showText $ tail text
+
+startupEditor :: Editor
+startupEditor = Editor { scrPos  = 0
+                       , scrSize = 10
+                       }
+
+initEditor :: IO (Editor)
+initEditor = return startupEditor
+
+fragment :: (Int, Int) -> [a] -> [a]
+fragment (start, end) arr
+  | start >= end = []
+  | length arr <= start = []
+  | otherwise = arr!!start : fragment ((start + 1), end) arr
+
+addLineNumber :: Int -> Text -> Text
+addLineNumber _ [] = []
+addLineNumber num (x:xs) = ((show num) ++ (' ' : x)) : addLineNumber (num + 1) xs
+
+addLineNumbers :: Text -> Text
+addLineNumbers = addLineNumber 1
+
+editorLoop :: Editor -> IO ()
+editorLoop editor = do
+  let raw = "All\nYour\nBase\nAre\nBelong\nTo\nUs"
+  let rawText = stringToText raw
+  let frag = fragment fragVal rawText
+  let text = addLineNumber ((fst fragVal) + 1) frag
+  showText text
+  if True then return () else editorLoop editor
+  where
+    fragVal = (scrPos editor, (scrPos editor) + scrSize editor)
+
 main :: IO ()
-main = putStrLn "All your base are belong to us!"
+main = do
+  editor <- initEditor
+  editorLoop editor
